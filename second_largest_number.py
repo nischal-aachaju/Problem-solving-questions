54a77d5b22e64da897de5d8e2f4031bf89c8153b
Nischal Shrestha <sthanischal5060@gmail.com>
2026-02-03 15:32:15 +0545

    second_largest number

--- second_largest_number.py
+++ second_largest_number.py
@@ -1,1 +1,9 @@
-print("Hello")
+
+lst= [10, 23, 45, 89,101 ,101,12]
+first_largest=max(lst)
+second_largest=min(lst)
+for i in lst:
+    if second_largest <= i and i < first_largest:
+        second_largest=i
+        
+print(second_largest)
