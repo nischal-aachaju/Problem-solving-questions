a535d9356421948b6b913e34d44cb20924f8fe5e
Nischal Shrestha <sthanischal5060@gmail.com>
2026-02-04 20:16:53 +0545

    second method (good )

--- sum_of_digits.py
+++ sum_of_digits.py
@@ -6,4 +6,17 @@ sum=0
 for i in str(num):
     sum=sum+int(i)
 
-print(sum)
+# print(sum)
+
+# second method
+
+n=1234567
+sum_n=0
+while True:
+    s=n%10
+    sum_n+=s
+    n=n//10
+    if n==0:
+        break
+
+print(sum_n)
