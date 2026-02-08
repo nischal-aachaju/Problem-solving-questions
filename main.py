c4e0d46992218861e9af23efd336c60478886896
Nischal Shrestha <sthanischal5060@gmail.com>
2026-02-08 20:49:29 +0545

    practicing question

---
+++ main.py
@@ -0,0 +1,36 @@
+# True = False
+# d = {0, 1, 2}
+# for x in d:
+#     print(x)
+# print(d)
+# print(type(type(int)))
+# a="d"
+# b=a.maketrans("as","bd")
+# print(chr(b[115]))
+# set1 = {3, 4, 5} 
+# set1.update([1, 2, 3]) 
+# print(set1)
+# set1 = {1, 2, 3} 
+# set2 = set1.add(4) 
+# print(set2)
+# set1 = {1, 2, 3} 
+
+# set2 = {4, 5, 6} 
+# print(len(set1 )+len(set2))\
+# from functools import reduce
+# lst=[1,2,3,4]
+# add_sub=reduce(lambda x,y:x+y,lst)
+# # a,b=add_sub(lst,lst)
+# print(add_sub)
+
+# try:
+#     a = int(input("Enter a:"))
+#     b = int(input("Enter b:"))
+#     c = a/b;
+#     print(c)
+# except Exception as e:
+#     print(e)
+# else:
+#     print("Hi I am else block")
+# finally:
+#     print("I am finally block")
