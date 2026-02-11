58affc6a4133de5ed7d83e825127c4f30d60d9a4
Nischal Shrestha <sthanischal5060@gmail.com>
2026-02-11 20:06:36 +0545

    next method

--- max_dict.py
+++ max_dict.py
@@ -1,11 +1,29 @@
+# data = {
+#     "Ram": 85,
+#     "Shyam": 92,
+#     "Hari": 78,
+#     "Sita": 95
+#     }
+
+# key = max(data, key=data.get)
+
+# print("Highest value key:", key)
+# print("Highest value:", data[key])
+
 data = {
     "Ram": 85,
     "Shyam": 92,
     "Hari": 78,
     "Sita": 95
-    }
+}
 
-key = max(data, key=data.get)
+max_key = None
+max_value = -1
 
-print("Highest value key:", key)
-print("Highest value:", data[key])
+for key in data:
+    if data[key] > max_value:
+        max_value = data[key]
+        max_key = key
+
+print("Highest value key:", max_key)
+print("Highest value:", max_value)
