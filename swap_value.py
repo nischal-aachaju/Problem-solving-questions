0f39849217d9edf80143a3ec9ed17eeb0bf432ec
Nischal Shrestha <sthanischal5060@gmail.com>
2026-02-23 21:13:45 +0545

    build in python method

--- swap_value.py
+++ swap_value.py
@@ -12,8 +12,19 @@ print(a,b)
 # method 2
 x=10
 y=20
+# before swap
 print(x,y)
 x=x-y
 y=x+y
 x=y-x
+# after swap
 print(x,y)
+
+# method 3 (Built in)
+p=20
+q=30
+# before swap
+print(p,q)
+p,q=q,p
+# after swap
+print(p,q)
