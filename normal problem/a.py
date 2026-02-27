e8a6d1b85780434771225bc92b7a94a240d7645e
Nischal Shrestha <sthanischal5060@gmail.com>
2026-02-27 20:32:24 +0545

    amstrong num

---
+++ normal problem/a.py
@@ -0,0 +1,13 @@
+
+# num=int(input("Enter number: "))
+# num_cpy=num
+# s=0
+# while num>0:
+#     r=num % 10
+#     s=s+r**3
+#     num=num//10
+# if s==num_cpy:
+#     print("armstrong")
+# else:
+#     print("not armstrong")
+
